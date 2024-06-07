fe2fd21ead9c4fe9e5861e768a7bf96377724f83
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-06-07 23:39:47 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,32 @@
+let vegetable:string[]=["gajar","moli","khera","methi","allo","palak",]
+console.log(vegetable[4]);
+console.log(vegetable.length);
+//push . say last main add hota
+vegetable.push("matar");
+console.log(vegetable);
+//pop. say last wala delet 
+vegetable.pop();
+console.log(vegetable);
+//.shift sy start wala delet
+vegetable.shift();
+console.log(vegetable);
+//.unshift sy start main add hoga
+vegetable.unshift("imli");
+console.log(vegetable);
+//slice k liy veraible bany gay phir .slice k bad jitna print chhy os ka index likhy gy
+let slice=vegetable.slice(2,4);
+console.log(slice);
+// splice k liy bhi alag virable bany ga phir jag ko define karny k bad
+// string main name likhy gy
+ let splice=vegetable.splice(2,2,"garpes,manogo",);
+console.log(vegetable);
+// tuples is main multipal data type lety or ossy define karty
+let fruittuples:[string,number,string,number]=["apple" ,2,"mango",4]
+console.log(fruittuples);
+
+
+
+
+
+
+
